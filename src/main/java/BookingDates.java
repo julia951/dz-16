ef50aae8703e2ecdc6d0ae2eaa1dd2921b9a3eb1
Julia <klimuk.julia@gmail.com>
2024-09-02 19:33:31 +0300

    add classes

---
+++ src/main/java/BookingDates.java
@@ -0,0 +1,25 @@
+public class BookingDates {
+    private Date checkin;
+    private Date checkout;
+
+    public BookingDates() {
+    }
+
+    // Getters and Setters
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(Date checkin) {
+        this.checkin = checkin;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(Date checkout) {
+        this.checkout = checkout;
+    }
+}
---
+++ src/main/java/BookingId.java
@@ -0,0 +1,16 @@
+public class BookingId {
+    private int bookingid;
+
+    public BookingId() {
+    }
+
+    // Getters and Setters
+
+    public int getBookingid() {
+        return bookingid;
+    }
+
+    public void setBookingid(int bookingid) {
+        this.bookingid = bookingid;
+    }
+}
